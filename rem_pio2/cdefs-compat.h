c6b2dd1497f36551240733cacd30254a99311509
Cesar Crusius <cesar@cariden.com>
2014-09-23 15:21:04 +0000

    Clean up Solaris 11 fixes, use GNU preprocessor definitions when available.

--- rem_pio2/cdefs-compat.h
+++ rem_pio2/cdefs-compat.h
@@ -3,7 +3,7 @@
 
 #if !defined(_WIN32) && !defined(__sun)
 #include "sys/cdefs.h"
-#else /* _WIN32 */
+#else /* _WIN32 || __sun */
 
 #if defined(__cplusplus)
 #define	__BEGIN_DECLS	extern "C" {
@@ -15,7 +15,7 @@
 
 #define _SYS_CDEFS_H_
 
-#endif /* _WIN32 */
+#endif /* _WIN32 || __sun */
 
 
 
